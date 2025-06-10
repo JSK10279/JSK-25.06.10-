8bd8a1d4fa22330f83d645f877f2968496729b0f
JSK10279 <jsk10279@gmail.com>
2025-06-10 14:46:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
 st.title("나의 첫 app")
 st.write("hello")
+name=st.text_imput("이름입력")
+if name:
+  if name=="JSK":
+    st.success("반갑습니다")
+  else:
+    st.warning("지정되지 않은 사용자입니다")
